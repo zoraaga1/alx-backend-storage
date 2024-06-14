af3cee896e1c50e1cf8d1f79f2e8367874aec6da
zoraaga1 <fati.sousse12@gmail.com>
2024-06-15 00:55:52 +0100

    task2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rabk country origins of bands by the number of fans
-SELECT origin, SUM(fans) AS nb_fans FROM bands GROUP BY
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY
     origin
 ORDER BY
-    b_fans DESC;
+    nb_fans DESC;
