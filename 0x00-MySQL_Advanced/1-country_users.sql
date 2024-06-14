4fc1dc69cc86eb81c56c0c8440a6bd6641036482
zoraaga1 <fati.sousse12@gmail.com>
2024-06-15 00:51:26 +0100

    task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,7 @@
 -- Creates a table users following these requirements:
 CREATE TABLE IF NOT EXISTS users (
-  
-)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
